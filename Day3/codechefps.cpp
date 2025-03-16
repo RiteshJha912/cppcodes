3919eedea3951478eeb6316215ab7527546edd43
RiteshJha912 <141942049+RiteshJha912@users.noreply.github.com>
2025-03-16 17:33:59 +0530

    day3 commit

---
+++ Day3/codechefps.cpp
@@ -0,0 +1,71 @@
+// 1
+// // replace the '_' with the correct syntax
+// #include <bits/stdc++.h>
+
+// using namespace std;
+
+// int main()
+// {
+//     int A, B, C, D, E, F, G, H, I;
+//     cin >> A >> B;
+//     cin >> C >> D >> E;
+//     cin >> F >> G >> H >> I;
+//     cout << A << " " << B << " " << C << " " << D << " " << E << " " << F << " " << G << " " << H << " " << I;
+//     return 0;
+// }
+
+
+
+// 2
+// // Update the '_' in the code below
+// #include <bits/stdc++.h>
+
+// using namespace std;
+
+// int main()
+// {
+//     string A, B, C, D, E, F, G, H, I;
+//     cin >> A >> B;
+//     cin >> C >> D >> E;
+//     cin >> F >> G >> H >> I;
+//     cout << A << " " << B << " " << C << " " << D << " " << E << " " << F << " " << G << " " << H << " " << I;
+//     return 0;
+// }
+
+
+
+// 3
+// // Update the '_' in the code below
+// #include <bits/stdc++.h>
+
+//     using namespace std;
+
+// int main()
+// {
+//     int a = 5;
+//     cout << a << endl;
+
+//     string b = "abcde";
+//     cout << b << endl;
+
+//     int array[] = {1, 2, 3};
+//     cout << array[0] << " " << array[1] << " " << array[2];
+// }
+
+
+
+// 4
+// // Update the '_' in the code below
+// #include <bits/stdc++.h>
+
+//     using namespace std;
+
+// int main()
+// {
+//     int N;
+//     string S;
+//     cin >> N;
+//     cin >> S;
+//     cout << N << " " << S;
+//     return 0;
+// }
