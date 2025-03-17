437c82fc3be71088608f33610b0701f95d4c3929
RiteshJha912 <141942049+RiteshJha912@users.noreply.github.com>
2025-03-17 21:32:39 +0530

    hmm

---
+++ Day4/chauthadin.cpp
@@ -0,0 +1,74 @@
+// 1
+// // Update the '_' in the code below
+// #include <bits/stdc++.h>
+
+//     using namespace std;
+
+// int main()
+// {
+//     int A, B, C, D, E;
+//     cin >> A;
+//     cin >> B;
+//     cin >> C;
+//     cin >> D;
+//     cin >> E;
+//     cout << A << endl;
+//     cout << B << endl;
+//     cout << C << endl;
+//     cout << D << endl;
+//     cout << E << endl;
+// }
+
+
+
+// 2
+// // Update the '_' in the code below
+// #include <bits/stdc++.h>
+
+//     using namespace std;
+
+// int main()
+// {
+//     int t;
+//     // accept the count of test cases given in the the 1st line
+//     cin >> t;
+//     // Run a loop to accept 't' inputs
+//     while (t--)
+//     {
+//         int N;
+//         // accept an integer N in each test case
+//         cin >> N;
+//         // output the number mirror for each test case
+//         cout << N << endl;
+//     }
+// }
+
+
+
+// 3
+// // Update the '_' below to solve the problem
+// #include <bits/stdc++.h>
+
+//     using namespace std;
+
+// int main()
+// {
+//     int t;
+//     // accept the count of test cases given in the the 1st line
+//     cin >> t;
+//     // Run a loop to accept 't' inputs
+//     while (t--)
+//     {
+//         int A,
+//             B,
+//             C,
+//             D,
+//             E;
+//         // accept 2 integers on the 1st line of each test case
+//         cin >> A >> B;
+//         // accept 3 integers on the 1st line of each test case
+//         cin >> C >> D >> E;
+//         // output the 5 integers on a single line for each test case
+//         cout << A << " " << B << " " << C << " " << D << " " << E << endl;
+//     }
+// }
